4ee247a733ddf5a32e1e8e20843c77112679dd5e
Amir Ali Tariq <amiralitariq@gmail.com>
2025-03-12 11:28:00 +0500

    Projects Completed

---
+++ 09_Website/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("Simple Data Dashboard")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Data Preview")
+    st.write(df.head())
+
